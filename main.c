1321885638b1beea6d2db04740dce999425cbf73
Kriegslustig <github@ls7.ch>
2015-04-12 17:30:22 +0200

    Test all util functions

--- main.c
+++ main.c
@@ -2,10 +2,16 @@
 
 int main ()
 {
-  char some_file[] = "test";
-  
-  fs_mkdir(some_file);
-
+  char some_file[] = "testfile";
+  char another_file[] = "testfile_cp";
+  char some_string[] = "SOmething or other!!";
+  write_string_to_file(some_file, some_string);
+  fs_cp(some_file, another_file);
+  print_array(read_string_from_file(another_file));
+  fs_rm(some_file);
+  printf("\n");
+  print_array(read_string_from_file(another_file));
+  fs_rm(another_file);
   printf("\n");
   return 0;
 }
