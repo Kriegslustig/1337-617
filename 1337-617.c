fa009912d9b1df5b73e5807a263e4541756b9dfb
Kriegslustig <github@ls7.ch>
2015-04-12 18:03:04 +0200

    Add definitons for some hash functions

--- 1337-617.c
+++ 1337-617.c
@@ -1,6 +1,27 @@
 #include <util.h>
 
 /*
+ * _h45h stands for hash
+ * it simply creates a random hash of the passed length
+ */
+int _h45h (
+  const int length,
+  char *restrict hash
+)
+{
+  return 1;
+} 
+
+/*
+ * c3c0_h45h stands for check hash
+ * Checks wether or not the passed hash has been used as a commit ID before
+ */
+int c3c0_h45h (const char *restrict hash)
+{
+  return 1;
+}
+
+/*
  * _1n17 is leet for init.
  * This function initializes a 1337-617 repo.
  * Meaning it creates a directory with the passed name
